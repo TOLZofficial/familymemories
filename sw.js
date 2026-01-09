df76a9cd047c4e286e5cdad35c1bf3ba8869237c
TOLZofficial <tolzofficial1@gmail.com>
2026-01-09 17:12:09 +0000

    Skip caching non-http requests

--- sw.js
+++ sw.js
@@ -34,6 +34,7 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", (event) => {
   const { request } = event;
   if (request.method !== "GET") return;
+  if (!request.url.startsWith("http")) return;
 
   event.respondWith(
     caches.match(request).then((cached) => {
